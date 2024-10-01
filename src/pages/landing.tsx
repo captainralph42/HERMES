f1b043a1151158a6574b1141d377c9e8f22064a2
captainralph42 <168578364+captainralph42@users.noreply.github.com>
2024-10-01 16:39:07 +0200

    add description on lp

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -27,6 +27,10 @@ const Landing = () => {
       </header>
 
       <div className={styles.content}>
+        <p className={styles.description}>
+        Welcome to Hermes, the dApp that combines fun and blockchain. Here, you can generate totally random punchlines and compliments—often sarcastic, always tied to the world of crypto. And the best part? Each punchline is minted as an NFT, so you get a unique piece to add to your wallet.
+        It’s simple: you adjust the sliders to tweak the tone (more humor, love, subtlety, or even the length), the AI does the rest, and your NFT is ready to be collected or shared. If you're looking for some laughs while riding the crypto wave, Hermes is the perfect place for you.
+        </p>
         <button onClick={handleAccessDApp} className={styles.accessButton}>
           Enter the realm
         </button>
