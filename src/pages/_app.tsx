b911a24a6ec29b480693912edd759d347ac3fc93
captainralph42 <168578364+captainralph42@users.noreply.github.com>
2024-10-01 17:06:30 +0200

    add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { AlephiumWalletProvider } from '@alephium/web3-react'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AlephiumWalletProvider theme="simple-dark" network="testnet" addressGroup={0}>
       <Component {...pageProps} />
+      <Analytics />
     </AlephiumWalletProvider>
   )
 }
