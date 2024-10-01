195117c4423a3127326f9751b62aa88c9cd35177
captainralph42 <168578364+captainralph42@users.noreply.github.com>
2024-10-01 21:26:12 +0200

    more responsivity

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { AlephiumWalletProvider } from '@alephium/web3-react'
-import { Analytics } from '@vercel/analytics/react'
+import React from 'react';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { AlephiumWalletProvider } from '@alephium/web3-react';
+import { Analytics } from '@vercel/analytics/react';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AlephiumWalletProvider theme="simple-dark" network="testnet" addressGroup={0}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <Component {...pageProps} />
       <Analytics />
     </AlephiumWalletProvider>
-  )
+  );
 }
